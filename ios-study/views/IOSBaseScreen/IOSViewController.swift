6d40b0f0b21845431fbfb4256b95f73f5f51fb3b
yw.hao <yw.hao@trip.com>
2024-06-12 13:46:46 +0800

    feat: add gemfile and update proj code

--- ios-study/views/IOSBaseScreen/IOSViewController.swift
+++ ios-study/views/IOSBaseScreen/IOSViewController.swift
@@ -9,36 +9,20 @@ import UIKit
 
 class IOSViewController: UIViewController {
     
-    @IBOutlet weak var number1: UITextField!
-    @IBOutlet weak var number2: UITextField!
-    @IBOutlet weak var sumButton: UIButton!
-    @IBOutlet weak var sumnumber: UILabel!
+    @IBOutlet weak var xibSubContainer: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        number1.text = "0"
-        number2.text = "0"
-    }
-    
-    @IBAction func compute() {
-        print("compute")
-        guard let num1Text = number1.text, let num1 = Int(num1Text) else {
-            print("Invalid input for number1")
-            return
-        }
-
-        guard let num2Text = number2.text, let num2 = Int(num2Text) else {
-            print("Invalid input for number2")
-            return
-        }
-        sumnumber.text = "\(num1 + num2)"
         
-        // 第一种做法
-//        number2.resignFirstResponder()
-//        number1.resignFirstResponder()
-        
-        self.view.endEditing(true)
+        var label1 = UILabel()
+        label1.text = "通过代码方式添加"
+        label1.textColor = .black
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(label1);
+        NSLayoutConstraint.activate([
+            label1.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            label1.topAnchor.constraint(equalTo: xibSubContainer.bottomAnchor, constant: 20)
+        ])
     }
 
 }
