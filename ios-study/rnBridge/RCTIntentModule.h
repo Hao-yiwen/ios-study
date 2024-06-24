9c3cd58655d4fd9dd6993fc95f59f5db210a4543
yw.hao <yw.hao@trip.com>
2024-06-24 13:32:38 +0800

    feat: add rnbridge

---
+++ ios-study/rnBridge/RCTIntentModule.h
@@ -0,0 +1,18 @@
+//
+//  RCTIntentModule.h
+//  ios-study
+//
+//  Created by yw.hao on 2024/6/24.
+//
+
+#ifndef RCTIntentModule_h
+#define RCTIntentModule_h
+
+#import <React/RCTBridgeModule.h>
+
+@interface RCTIntentModule : NSObject <RCTBridgeModule>
+
+@end
+
+
+#endif /* RCTIntentModule_h */
